e07d0c9dad30abd96dbf789a653a65e55c3e1e49
Fabianus Hendy Evan <hendyevan@gmail.com>
2019-05-02 18:28:32 +0700

    change code

--- Clean Swift/Custom Views/Detail Movie/MovieDescriptionTableViewCell.swift
+++ Clean Swift/Custom Views/Detail Movie/MovieDescriptionTableViewCell.swift
@@ -11,17 +11,17 @@ import Kingfisher
 
 class MovieDescriptionTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var backdropImageView: UIImageView!
-    @IBOutlet weak var posterImageView: UIImageView!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var ratingLabel: UILabel!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var categoryLabel: UILabel!
+//    @IBOutlet weak var backdropImageView: UIImageView!
+//    @IBOutlet weak var posterImageView: UIImageView!
+//    @IBOutlet weak var descriptionLabel: UILabel!
+//    @IBOutlet weak var ratingLabel: UILabel!
+//    @IBOutlet weak var titleLabel: UILabel!
+//    @IBOutlet weak var categoryLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        posterImageView.round(withRadius: 10)
+//        posterImageView.round(withRadius: 10)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -31,13 +31,13 @@ class MovieDescriptionTableViewCell: UITableViewCell {
     }
     
     func setupCell(movie: MovieModel.Movie2){
-        let _backdropURL = "http://image.tmdb.org/t/p/w780\(movie.backdropPath)"
-        let _posterURL = "http://image.tmdb.org/t/p/w780\(movie.posterPath)"
-        backdropImageView.kf.setImage(with: URL(string: _backdropURL))
-        posterImageView.kf.setImage(with: URL(string: _posterURL))
-        titleLabel.text = movie.title
-        ratingLabel.text = "\(movie.voteAverage)"
-        descriptionLabel.text = movie.overview
+//        let _backdropURL = "http://image.tmdb.org/t/p/w780\(movie.backdropPath)"
+//        let _posterURL = "http://image.tmdb.org/t/p/w780\(movie.posterPath)"
+//        backdropImageView.kf.setImage(with: URL(string: _backdropURL))
+//        posterImageView.kf.setImage(with: URL(string: _posterURL))
+//        titleLabel.text = movie.title
+//        ratingLabel.text = "\(movie.voteAverage)"
+//        descriptionLabel.text = movie.overview
     }
     
 }
